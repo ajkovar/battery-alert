34bd85e073a01ae62f788b8f5e7b65e2bde57820
Alex Kovar <ajkovar@gmail.com>
2021-02-21 12:06:56 -0600

    added notification

--- Main.hs
+++ Main.hs
@@ -1,7 +1,10 @@
-import System.Process (readProcess)
+import System.Process (readProcess, runCommand)
 import Data.List (find, stripPrefix, isPrefixOf)
 import Data.Char (isSpace)
 import Data.Text (strip, pack, unpack)
+import Text.Printf (printf)
+import System.Process.Internals (ProcessHandle(ProcessHandle))
+import Control.Monad.Trans.Maybe (MaybeT(MaybeT, runMaybeT))
 
 strip' :: String -> String
 strip' = unpack . strip . pack
@@ -12,7 +15,21 @@ readBatteryInfo = readProcess "system_profiler" ["SPPowerDataType"] []
 prefix :: String
 prefix = "State of Charge (%): "
 
-readBatteryState :: IO (Maybe Float)
-readBatteryState = do
+readBatteryLevel :: IO (Maybe Float)
+readBatteryLevel = do
     stats <- fmap strip' . lines <$> readBatteryInfo
     return $ read <$> (find (isPrefixOf prefix) stats >>= stripPrefix prefix)
+
+notificationBody :: Float -> String
+notificationBody = printf "Battery level is at %.0f%%.  Time to unplug!"
+
+notifyCommand :: Float -> String
+notifyCommand level =
+    printf "osascript -e 'display notification \"%s\" with title \"%s\"'"
+           (notificationBody level)
+           "Unplug Computer"
+
+main :: IO (Maybe ProcessHandle)
+main = runMaybeT $ do
+  level <- MaybeT readBatteryLevel
+  MaybeT $ Just <$> runCommand (notifyCommand level)
